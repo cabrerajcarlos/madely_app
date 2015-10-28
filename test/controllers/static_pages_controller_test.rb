59dbc1a401275fe867ed53d95c1bb3fdca1da8fd
Juan Carlos Cabrera <cabrera01@gmail.com>
2015-10-27 22:13:42 -0400

    Refactoring

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Madely Application"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
